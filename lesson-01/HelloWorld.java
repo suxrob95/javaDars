72ec76908cd51e44ce62df4180163949320d3392
suxrob95 <sukhrobmurotov@gmail.com>
2026-05-19 06:50:57 -0700

    ozgarishlar1

---
+++ lesson-01/HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Salom, Java!");
+        System.out.println("Bu mening 1-darsim.");
+    }
+}
---
+++ lesson-02/JavaIf.java
@@ -0,0 +1,6 @@
+public class JavaIf {
+    public static void main(String[] args) {
+        System.out.println("Salom, Java!");
+        System.out.println("Bu mening 1-darsim.");
+    }
+}
